d2fb3de11afdcbdc4025f401bba2c98ec1c552c4
Tara Mahoney <tara.mahoney@me.com>
2015-05-13 17:38:03 -0400

    Resolved merge conflicts in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,9 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-
 
   root 'projects#index'
 
   resources :projects
-=======
-  
-  root"projects#index"
-  resources :projects
 
-
-
-
->>>>>>> new-project-form
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
