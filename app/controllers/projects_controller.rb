310bfa69b504a61afd418ef7159242a6465ecf28
Iva Delic <ms.iva.delic@gmail.com>
2015-05-13 17:27:58 -0400

    Search works for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,31 @@
+
 class ProjectsController < ApplicationController
 
 	def index
-		@projects = Project.all
+			@projects = if params[:search]
+			Project.where("LOWER(title) LIKE LOWER(?)", "%#{params[:search]}%")
+		else 
+			Project.all
+		end
+
+		@projects = @projects.order("projects.created_at DESC").page(params[:page])
+
+		if request.xhr?
+			render @projects
+		end
 	end
 
+	private 
+	def project_params
+		params.require(:project).permit(
+			:title,
+			:description, 
+			:start_time, 
+			:end_time, 
+			:category, 
+			:tags, 
+			:rewards_attributes [:id, :title, :backer_limit, :description, :amount, :destroy]
+			)
+	end
 end
+
