59f02b5ecdfbb03113a1afcb23416ba5829080d4
Tara Mahoney <tara.mahoney@me.com>
2015-05-13 17:31:58 -0400

    Add new Project form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,31 @@
 class ProjectsController < ApplicationController
+
+  def new
+   @project = Project.create
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to projects_path, notice: "Project created successfully!"
+    else
+      render 'new', alert: "Something went wrong! Try again?"
+    end
+  end 
+
+private
+  def project_params
+    params.require(:project).permit(
+      :title,
+      :description,
+      :start_time,
+      :end_time,
+      :category,
+      :tags,
+      :goal_amount, 
+      rewards_attributes: [:id, :amount, :title, :description, :backer_limit, :_destroy]
+    )
+  end
+
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,6 @@ class Project < ActiveRecord::Base
   has_many :pledges, through: :rewards
   has_many :rewards
   belongs_to :user
+
+  accepts_nested_attributes_for :rewards, allow_destroy: true
 end
