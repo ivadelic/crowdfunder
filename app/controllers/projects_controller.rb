a055b444c7f6ab740f18ba20870de0fc0e8fad77
Dan Comand <dcomand@gmail.com>
2015-05-13 16:22:48 -0400

    added show.html.erb with faker gem.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,29 @@
 class ProjectsController < ApplicationController
+
+	def index
+		@project = Project.all
+	end
+
+	def show
+		@project = Project.find(params[:id])
+	end
+
+
+	private
+	def project_params
+	
+		params.require(:project).permit(
+			:title, 
+			:description, 
+			:start_time, 
+			:end_time, 
+			:category, 
+			:tags
+			)
+
+	end
+
+
+
+
 end
