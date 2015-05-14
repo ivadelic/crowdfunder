046a191754866f43614997b4c506c9d3b48f9cae
Iva Delic <ms.iva.delic@gmail.com>
2015-05-14 16:09:29 -0400

    Added login function with restriction in making a project

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+	skip_before_filter :require_login, only: [:index, :new, :create]
+
+	if @user.save
+		redirect_to(:users, notice: "User was successfully created")
+	end 
+
+	private 
+
+	def user_params
+		params.require(:user).permit(:email, :password, :password_confirmation)
+	end 
 end
