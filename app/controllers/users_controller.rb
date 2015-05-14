8874736a6b0993cbeac1fd8a2528167ccf938d43
Dan Comand <dcomand@gmail.com>
2015-05-14 15:03:20 -0400

    added user sign model and controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+
+	def new
+
+		@user = User.new
+
+	end
+
+	def create
+
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to projects_url, notice: "Signed Up & Logged In!"
+			auto_login(@user)
+		else
+			render "new", alert: "There was an error! Try again"
+		end
+	end
+
+	private
+		def user_params
+			params.require(:user).permit(:email, :password, :password_confirmation)
+		end
 end
