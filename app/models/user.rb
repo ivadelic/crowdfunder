b1374073a806d81129662a568d04d05281cc8bdf
Iva Delic <ms.iva.delic@gmail.com>
2015-05-14 18:05:44 -0400

    Added seeds for amount of projects, user info and rewards info. Look at seeds file. Also added user names to index and show pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,11 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
   validates :first_name, :last_name, :email, :password, presence: true
-  validates_confirmation_of :password, :message => "should match confirmation", :if => :password
 
   has_many :owned_projects, class_name: "Project"
   has_many :backed_projects, through: :pledges, class_name: "Project"
   has_many :pledges
 
   validates :password, length: { minimum: 7 }
-  validates :password, confirmation: true
-  validates :password_confirmation, presence: true
   validates :email, uniqueness: true 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,36 @@
-100.times do 
 
-	Project.create(
-	title: Faker::Commerce.product_name,
-  description: Faker::Hacker.say_something_smart,
-
-      )
-
+10.times do
+	User.create!(
+		first_name: Faker::Internet.user_name,
+    last_name: Faker::Internet.user_name,
+    email: Faker::Internet.safe_email,
+    password: 'password'
+	)
+	print "U"
 end
+
+users = User.all
+
+
+20.times do
+
+	pr = Project.create!(
+		title: Faker::Commerce.product_name,
+	  description: Faker::Hacker.say_something_smart,
+  	user: users.sample,
+  	goal_amount: Faker::Commerce.price
+  )
+  print "P"
+
+  10.times do |n|
+		pr.rewards.create!(
+			title: Faker::Commerce.product_name,
+     	description: Faker::Hacker.say_something_smart,
+    	backer_limit: Faker::Number.number(3),
+    	amount: 15*(3**n)
+		)
+		print "R"
+	end
+end
+
+
