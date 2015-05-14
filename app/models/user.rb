9bdb0e85056eda8eb661bbc6c2416a6a21629f21
Dan Comand <dcomand@gmail.com>
2015-05-14 16:01:00 -0400

    fixed error with password confirmation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
-  validates :first_name, :last_name, :email, :password, :password_confirmation, presence: true
+  validates :first_name, :last_name, :email, :password, presence: true
+  validates_confirmation_of :password, :message => "should match confirmation", :if => :password
+
   has_many :owned_projects, class_name: "Project"
   has_many :backed_projects, through: :pledges, class_name: "Project"
   has_many :pledges
