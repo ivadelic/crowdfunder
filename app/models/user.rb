edc19edc858d68cc458ecd75bc91eadb220acfa9
Dan Comand <dcomand@gmail.com>
2015-05-14 15:42:19 -0400

    wrote validations for new user sign up form.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+  validates :first_name, :last_name, :email, :password, :password_confirmation, presence: true
   has_many :owned_projects, class_name: "Project"
   has_many :backed_projects, through: :pledges, class_name: "Project"
   has_many :pledges
