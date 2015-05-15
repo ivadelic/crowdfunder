ccd72964755d16b146a128600b8b314f0f87d3d2
Dan Comand <dcomand@gmail.com>
2015-05-15 15:37:58 -0400

    error fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
   validates :first_name, :last_name, :email, :password, presence: true
-
+  validates_confirmation_of :password, :message => "should match confirmation", :if => :password
   has_many :owned_projects, class_name: "Project"
   has_many :backed_projects, through: :pledges, class_name: "Project"
   has_many :pledges
