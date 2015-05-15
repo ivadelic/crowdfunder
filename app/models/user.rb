926c278391c7ad01cdbbbe2f13637af3eb78600b
Dan Comand <dcomand@gmail.com>
2015-05-15 15:59:58 -0400

    Fixed some project stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,7 @@ class User < ActiveRecord::Base
   authenticates_with_sorcery!
   validates :first_name, :last_name, :email, :password, presence: true
   validates_confirmation_of :password, :message => "should match confirmation", :if => :password
-<<<<<<< HEAD
-=======
 
->>>>>>> 013f12e77433220b938135688ded4e0b26f392e9
   has_many :owned_projects, class_name: "Project"
   has_many :backed_projects, through: :pledges, class_name: "Project"
   has_many :pledges
